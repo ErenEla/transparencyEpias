6bd0243395850409a6c1b8b177bbf3fc6534ee34
ErenEla <ela.eren1@gmail.com>
2020-05-25 15:27:25 +0300

    python version has been changed

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="transparencyEPIAS", # Replace with your own username
+    name="transparency_epias", # Replace with your own username
     version="0.0.1",
     author="Eren Ela",
     author_email="ela.eren1@gmail.com",
@@ -18,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8.2',
+    python_requires='>=3',
 )
 
 #source packing.python
