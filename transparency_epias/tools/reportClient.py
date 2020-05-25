8e8ce4e3956dc7b0591cb4b5f5c73f7823c6a891
ErenEla <ela.eren1@gmail.com>
2020-05-25 12:27:11 +0300

    Price reports has been added

--- transparency_epias/tools/reportClient.py
+++ transparency_epias/tools/reportClient.py
@@ -3,11 +3,14 @@ import requests
 import json
 from datetime import timedelta 
 from datetime import datetime
-from markets import dayaheadClient
-from markets import validate as val
-from consumption import consumptionClient
+from transparency_epias.markets import dayaheadClient as dc
+from transparency_epias.markets import validate as val
+from transparency_epias.markets import balancingPowerClient as bpc
+from transparency_epias.markets import intradayClient as ic
+from transparency_epias.consumption import consumptionClient
+import xlsxwriter 
 
-class mcpClient:
+class reportsClient:
 
     def daily_excel_export(self):
         
@@ -21,7 +24,7 @@ class mcpClient:
         if current_date.hour >= int_announce_hour and current_date.hour < announce_hour:
             current_date = current_date + timedelta(days=1)
             current_date_formatted = datetime.strftime(current_date,'%Y-%m-%d')
-            mcp_list = dayaheadClient.dayahead.mcp_interim(current_date_formatted)
+            mcp_list = dc.dayahead.mcp_interim(current_date_formatted)
             print('Official MCP values are not published yet, for approved values try again after 2:00 P.M')
             mcp_df = pd.DataFrame({'Date':mcp_list[0],
                                 'MCP':mcp_list[2]
@@ -29,12 +32,12 @@ class mcpClient:
         elif current_date.hour >= 14:
             current_date = current_date + timedelta(days=1)
             current_date_formatted = datetime.strftime(current_date,'%Y-%m-%d')
-            mcp_list = dayaheadClient.dayahead.mcp(current_date_formatted,current_date_formatted)
+            mcp_list = dc.dayahead.mcp(current_date_formatted,current_date_formatted)
             mcp_df = pd.DataFrame({'Date':mcp_list[0],
                                 'MCP':mcp_list[1]
                                 })
         else:
-            mcp_list = dayaheadClient.dayahead.mcp(current_date_formatted,current_date_formatted)
+            mcp_list = dc.dayahead.mcp(current_date_formatted,current_date_formatted)
             mcp_df = pd.DataFrame({'Date':mcp_list[0],
                                     'MCP':mcp_list[1]
                                     })
@@ -44,7 +47,7 @@ class mcpClient:
 
     def excel_export(self, startDate, endDate):
 
-        mcp_list = dayaheadClient.dayahead.mcp(startDate, endDate)
+        mcp_list = dc.dayahead.mcp(startDate, endDate)
 
         mcp_df = pd.DataFrame({'Date':mcp_list[0],
                                 'MCP':mcp_list[1]
@@ -52,34 +55,73 @@ class mcpClient:
         mcp_df.to_excel(f'MCP_{startDate}_{endDate}.xlsx')
 
         print('Report Created Successfully!')
-        
-mcp = mcpClient()
 
-'''
-class conReportClient:
-
-    def weekly_consumption_trend(self, week_count):
+    def compare_price(self):
 
         current_date = datetime.today()
 
-        current_date_formatted = datetime.strftime(current_date, '%Y-%m-%d')
+        yest_date = current_date - timedelta(days=1)
 
-        yesterday_date = current_date_formatted - timedelta(days=-1)
-    
-        week_start = yesterday_date - timedelta(days=-7*week_count)
+        yest_date_formatted = datetime.strftime(yest_date,'%Y-%m-%d')
 
-        amount_list = consumptionClient.consumption.consumption_realtime(week_start,yesterday_date)[1]
-        date_list = consumptionClient.consumption.consumption_realtime(week_start,yesterday_date)[0]
+        mcp = dc.dayahead.mcp(yest_date_formatted, yest_date_formatted)[1]
+        smp = bpc.balancingPower.smp(yest_date_formatted, yest_date_formatted)[2]
+        wap = ic.intraday.weighted_average_price(yest_date_formatted, yest_date_formatted)[1]
+        date = dc.dayahead.mcp(yest_date_formatted, yest_date_formatted)[0]
 
-        weekly_amount = []
-
-        for week in range(1, week_count):
-            weekly_total = sum(amount_list[0:24*7*week])
-            weekly_amount.append(weekly_total)
+        df = pd.DataFrame({'Date':date,
+                                'MCP':mcp,
+                                'SMP':smp,
+                                'WAP':wap,
+                                })
         
-        for totals in weekly_amount:
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        excel_file = 'PriceComparison.xlsx'
+        sheet_name = 'Sheet1'
 
-'''
+        writer = pd.ExcelWriter(excel_file, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name=sheet_name)
+
+        # Access the XlsxWriter workbook and worksheet objects from the dataframe.
+        workbook = writer.book
+        worksheet = writer.sheets[sheet_name]
+
+        # Create a chart object.
+        chart = workbook.add_chart({'type': 'line'})
+
+        # Configure the series of the chart from the dataframe data.
+
+        name_list = ['mcp', 'smp','wap']
+        line_list = ['solid','dash_dot','round_dot']
+
+        for i in range(len(name_list)):
+            
+            col = i+2
+
+            chart.add_series({
+                'name': ['Sheet1',0,col],
+                'categories': ['Sheet1', 1, 0, 24, 0],
+                'values':     ['Sheet1', 1, col, 24, col],
+                'line':   {'dash_type': f'{line_list[i]}'}
+            })
+
+        # Configure the chart axes.
+        chart.set_x_axis({'name': 'Index', 'position_axis': 'on_tick'})
+        chart.set_y_axis({'name': 'Value', 'major_gridlines': {'visible': False}})
+
+        # Turn off chart legend. It is on by default in Excel.
+        chart.set_legend({'position': 'bottom'})
+
+        # Insert the chart into the worksheet.
+        worksheet.insert_chart('G2', chart, {'x_scale': 3, 'y_scale': 1.5})
+
+        # Close the Pandas Excel writer and output the Excel file.
+        writer.save()
+
+        print('Price Comparison excel is created!')
+
+
+reports = reportsClient()
 
 
 
