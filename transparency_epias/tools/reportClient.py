9264e33d260314897f7c8818a45e8e490879b33b
ErenEla <ela.eren1@gmail.com>
2020-06-06 19:53:09 +0300

    turkish holiday added

--- transparency_epias/tools/reportClient.py
+++ transparency_epias/tools/reportClient.py
@@ -9,6 +9,7 @@ from transparency_epias.markets import balancingPowerClient as bpc
 from transparency_epias.markets import intradayClient as ic
 from transparency_epias.consumption import consumptionClient
 import xlsxwriter 
+from transparency_epias.markets import validate as val
 
 class reportsClient:
 
@@ -124,8 +125,28 @@ class reportsClient:
 reports = reportsClient()
 
 
-# ISO format to datetime will be added
-# write sql function will be added 
-# Turkey calendar dates (holidays) will be added
+class calendarClient:
+
+    def get_holidays(self, startDate, endDate):
+
+        print('Warning! Holidays date range is between 2016 and 2020. Date range will be updated in future')
+
+        holidaysTR = {'Date':["2016-01-01","2016-04-23","2016-05-01","2016-05-19","2016-07-04","2016-07-05","2016-07-06","2016-07-07","2016-08-30","2016-09-11","2016-09-12","2016-09-13","2016-09-14","2016-09-15","2016-10-28","2016-10-29","2017-01-01","2017-04-23","2017-05-01","2017-05-19","2017-06-24","2017-06-25","2017-06-26","2017-06-27","2017-07-15","2017-08-30","2017-08-31","2017-09-01","2017-09-02","2017-09-03","2017-09-04","2017-10-28","2017-10-29","2018-01-01","2018-04-23","2018-05-01","2018-05-19","2018-06-14","2018-06-15","2018-06-16","2018-06-17","2018-07-15","2018-08-20","2018-08-21","2018-08-22","2018-08-23","2018-08-24","2018-08-30","2018-10-28","2018-10-29","2019-01-01","2019-04-23","2019-05-01","2019-05-19","2019-06-03","2019-06-04","2019-06-05","2019-06-06","2019-07-15","2019-08-10","2019-08-11","2019-08-12","2019-08-13","2019-08-14","2019-08-30","2019-10-28","2019-10-29","2020-01-01","2020-04-23","2020-05-01","2020-05-19","2020-05-23","2020-05-24","2020-05-25","2020-05-26","2020-07-15","2020-07-30","2020-07-31","2020-08-01","2020-08-02","2020-08-03","2020-08-30","2020-10-28","2020-10-29"],
+              'Days':[1,23,1,19,4,5,6,7,30,11,12,13,14,15,28,29,1,23,1,19,24,25,26,27,15,30,31,1,2,3,4,28,29,1,23,1,19,14,15,16,17,15,20,21,22,23,24,30,28,29,1,23,1,19,3,4,5,6,15,10,11,12,13,14,30,28,29,1,23,1,19,23,24,25,26,15,30,31,1,2,3,30,28,29],
+              'Holidays':["Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Religious Holiday","Eve","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Religious Holiday","Eve","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Eve","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Religious Holiday","Religious Holiday","Religious Holiday","Religious Holiday","Bank Holiday","Eve","Bank Holiday"] }
+
+        val.date_check(startDate,endDate)
+
+        # date list range check for parameters should be added
+        date_list = []
+        holiday_list = []
+        for d_index, dates in enumerate(holidaysTR['Date']):
+            if startDate <= dates <= endDate:
+                date_list.append(dates)
+                holiday_list.append(holidaysTR['Holidays'][d_index])
+
+        return date_list, holiday_list
+
+calendar = calendarClient()
 
 
